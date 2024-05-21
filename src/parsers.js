a621dfd0d2531d452873cbfbb64a6cd05467b2b1
ekaterinamavliutova <akumaaya@gmail.com>
2024-05-21 19:24:36 +0200

    refactor parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,21 +1,19 @@
 import yaml from 'js-yaml';
 
-// const parseJson = (text) => JSON.parse(text);
-// const parseYaml = (text) => yaml.load(text);
-
 const parsers = {
   json: JSON.parse,
-  yml: yaml.load,
   yaml: yaml.load,
 };
 
-export default (fileExtension) => {
+const getParser = (dataType) => {
   try {
-    if (parsers[fileExtension] === undefined) {
-      throw new Error(`'${fileExtension}' parsing is not supported`);
+    if (parsers[dataType] === undefined) {
+      throw new Error(`'${dataType}' parsing is not supported`);
     }
-    return parsers[fileExtension];
+    return parsers[dataType];
   } catch (err) {
     throw new Error(err);
   }
 };
+
+export default getParser;
